bb2f905dc3efad8503a732eae520bd3771e0e4e2
AlfredCYL <alfred.yl@outlook.com>
2024-09-06 11:01:06 +0800

    Version Upgrade

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setup(
     name='factor_analyser',  
-    version='1.0',  
+    version='1.1',  
     author='AlfredCYL',  
     author_email='alfred.yl@outlook.com', 
     description='A tool for factor development and analysis.',  
